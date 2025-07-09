1c38fe6572906c1fbd665859c29e7f1afcd2b523
Ilya Sokolov <>
2025-07-09 23:05:11 +0300

    tests for one and two roots

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class Program
+{
+    public static void Main() { }
+
+    public static IEnumerable<double> Solve(double a, double b, double c)
+    {
+        //ax^2+bx+c=0
+        var D = (b * b) - 4 * a * c;
+
+        if (D < 0)
+            return Enumerable.Empty<double>();
+
+        if (D == 0)
+        {
+            var x = (-b + Math.Sqrt(D)) / 2 * a;
+            return new double[] { x };
+        }
+
+        var x1 = (-b + Math.Sqrt(D)) / 2 * a;
+        var x2 = (-b - Math.Sqrt(D)) / 2 * a;
+
+        return new double[] { x1, x2 };
+    }
+}
