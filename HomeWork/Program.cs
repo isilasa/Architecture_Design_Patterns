7530e8d51cb496f95b036bfe8aa41bd805ba5e22
Ilya Sokolov <>
2025-07-11 22:47:26 +0300

    tests for solve function

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -4,13 +4,27 @@
 
     public static IEnumerable<double> Solve(double a, double b, double c)
     {
+        if (a.CompareTo(0) <= 0)
+            throw new ArgumentNullException("a con not be 0");
+
+
+        if (double.IsInfinity(a) || double.IsNegativeInfinity(a) || double.IsNaN(a))
+            throw new ArgumentNullException("a is incorrect");
+
+        if (double.IsInfinity(b) || double.IsNegativeInfinity(b) || double.IsNaN(b))
+            throw new ArgumentNullException("a is incorrect");
+
+        if (double.IsInfinity(c) || double.IsNegativeInfinity(c) || double.IsNaN(c))
+            throw new ArgumentNullException("a is incorrect");
+
+
         //ax^2+bx+c=0
         var D = (b * b) - 4 * a * c;
 
-        if (D < 0)
+        if (D.CompareTo(0) < 0)
             return Enumerable.Empty<double>();
 
-        if (D == 0)
+        if (D.CompareTo(0) == 0)
         {
             var x = (-b + Math.Sqrt(D)) / 2 * a;
             return new double[] { x };
